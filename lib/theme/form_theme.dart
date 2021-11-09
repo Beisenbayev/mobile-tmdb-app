d008382ffb451a1458c9c5eafa845dd645a7abbd
beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-11-09 22:26:06 +0600

    theme [init / main themes for application]

---
+++ lib/theme/form_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class InputFieldStyleTheme {
+  static const InputDecoration outlined = InputDecoration(
+    border: OutlineInputBorder()
+  );
+}
---
+++ lib/theme/text_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class TextStyleTheme {
+  static const TextStyle main = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 20,
+    color: Colors.black
+  );
+}
