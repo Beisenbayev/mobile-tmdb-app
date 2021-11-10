0f90e478ddc68982f4531cabb602722863d2e8ca
beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-11-10 08:05:26 +0600

    login-page [makeup / registration and verification email buttons added]

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:moovee_land/widgets/login_form_widget.dart';
+import 'package:moovee_land/theme/text_theme.dart';
+import 'package:moovee_land/theme/button_theme.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -27,9 +29,41 @@ class _LoginPageState extends State<LoginPage> {
         child: Column(
           children: <Widget>[
             LoginFormWidget(),
+            const SizedBox(height: 26.0),
+            _LoginPageTextWidget()
           ],
         ),
       ),
+    );
+  }
+}
+
+class _LoginPageTextWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Text(
+          'In order to use the editing and rating capabilities of TMDB, as well as get personal recommendations you will need to login to your account. If you do not have an account, registering for an account is free and simple. Click here to get started.: ',
+          style: TextStyleTheme.main,
+        ),
+        TextButton(
+          style: ButtonStyleTheme.textButton,
+          onPressed: () {},
+          child: Text('registration'),
+        ),
+        const SizedBox(height: 16.0),
+        const Text(
+          'If you signed up but didn\'t get your verification email, click here to have it resent: ',
+          style: TextStyleTheme.main,
+        ),
+        TextButton(
+          style: ButtonStyleTheme.textButton,
+          onPressed: () {},
+          child: Text('verification'),
+        )
+      ],
     );
   }
 }
