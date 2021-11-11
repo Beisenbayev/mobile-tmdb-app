f207695cce589d358ddfec9c62204b99e67b9618
beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-11-11 08:40:16 +0600

    style [colors / color palette was added]

---
+++ lib/theme/colors_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorPaletteTheme {
+  static const mainColor = Color.fromRGBO(3, 37, 65, 1);
+  static const secondaryColor = Color.fromRGBO(1, 180, 228, 1);
+
+  static const backgroundColor = Colors.white;
+  static const foregroundColor = Colors.white;
+
+  static const errorColor = Color.fromRGBO(172, 84, 84, 1);
+}
