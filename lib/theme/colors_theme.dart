8679c5a494b18146e352123bde020e70d5a1430c
beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-11-15 15:44:57 +0600

    theme [colors / radial-rercent-widget colors were added]

--- lib/theme/colors_theme.dart
+++ lib/theme/colors_theme.dart
@@ -10,6 +10,10 @@ abstract class ColorPaletteTheme {
 
   static const backgroundDark = Color.fromRGBO(27, 22, 26, 1.0);
 
+  static const radialPercentFill = Color.fromRGBO(8, 28, 34, 1.0);
+  static const radialPercentFree = Color.fromRGBO(28, 63, 39, 1.0);
+  static const radialPercentActive = Color.fromRGBO(33, 208, 122, 1.0);
+
   static const error = Color.fromRGBO(172, 84, 84, 1);
   static const subtitle = Color.fromRGBO(153, 153, 153, 1);
 }
