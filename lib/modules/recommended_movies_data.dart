007148ab06b701ef7dc38368f1c529e515d3a216
beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-11-16 22:18:07 +0600

    module [recommended-movies / recommended-movies data was created]

---
+++ lib/modules/recommended_movies_data.dart
@@ -0,0 +1,22 @@
+class RecommendedMovie {
+  final String title;
+  final String imageName;
+  final int userScore;
+
+  RecommendedMovie({
+    required this.title,
+    required this.imageName,
+    required this.userScore,
+  });
+}
+
+class RecommendedMoviesCollection {
+  static List<RecommendedMovie> recommendedMovie = [
+    RecommendedMovie(title: 'Dune', imageName: 'assets/images/recommendationMovie.jpg', userScore: 78),
+    RecommendedMovie(title: 'Dune', imageName: 'assets/images/recommendationMovie.jpg', userScore: 78),
+    RecommendedMovie(title: 'Dune', imageName: 'assets/images/recommendationMovie.jpg', userScore: 78),
+    RecommendedMovie(title: 'Dune', imageName: 'assets/images/recommendationMovie.jpg', userScore: 78),
+    RecommendedMovie(title: 'Dune', imageName: 'assets/images/recommendationMovie.jpg', userScore: 78),
+    RecommendedMovie(title: 'Dune', imageName: 'assets/images/recommendationMovie.jpg', userScore: 78),
+  ];
+}
