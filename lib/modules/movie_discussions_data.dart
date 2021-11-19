4984eb4c0099e4596231d37d8ed19c567e98debb
beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-11-19 23:24:32 +0600

    module [discussions / movie_discussions_data was added]

---
+++ lib/modules/movie_discussions_data.dart
@@ -0,0 +1,48 @@
+class Discussion {
+  final String title;
+  final String imageName;
+  final String username;
+  final String date;
+  final String type;
+  final int repliesCount;
+
+  Discussion({
+    required this.title,
+    required this.imageName,
+    required this.username,
+    required this.date,
+    required this.type,
+    required this.repliesCount,
+  });
+}
+
+class DiscussionsCollection {
+  final List<Discussion> discussions = [
+    Discussion(
+      title:
+          'Share your initial thoughts about No Time To Die *HEAVY SPOILER DISCUSSION*',
+      imageName: 'assets/images/discusserImage.jpg',
+      username: 'mechajutaro',
+      date: 'Nov 15, 2021 at 10:25 PM',
+      type: 'open',
+      repliesCount: 12,
+    ),
+    Discussion(
+      title: 'This Bond (also) kind of sucks',
+      imageName: 'assets/images/discusserImage.jpg',
+      username: 'mechajutaro',
+      date: 'Nov 15, 2021 at 10:25 PM',
+      type: 'open',
+      repliesCount: 32,
+    ),
+    Discussion(
+      title:
+          'Share your initial thoughts about No Time To Die *HEAVY SPOILER DISCUSSION*',
+      imageName: 'assets/images/discusserImage.jpg',
+      username: 'mechajutaro',
+      date: 'Nov 15, 2021 at 10:25 PM',
+      type: 'open',
+      repliesCount: 0,
+    ),
+  ];
+}
