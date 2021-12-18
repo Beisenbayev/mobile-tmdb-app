0d517dfd56ca9d898cc6f2fec0ce417b1e8bd737
beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-12-18 23:57:53 +0600

    login-page model submit handler

--- lib/core/models/login_page_model.dart
+++ lib/core/models/login_page_model.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:moovee_land/client_api/web/auth_web_service.dart';
 
 class LoginPageModel extends ChangeNotifier {
-  
+  final _authService = AuthWebService();
+  final loginController = TextEditingController(text: '');
+  final passwordController = TextEditingController(text: '');
+  bool isSubmitting = false;
+  String? errorText;
+
+  void handleLogin(BuildContext context) {
+    errorText = null;
+    final login = loginController.text;
+    final password = passwordController.text;
+
+    if (login == '' || password == '') {
+      errorText = 'Fields cannot be empty!';
+      notifyListeners();
+      return;
+    }
+
+    try {
+      _authService.auth(username: login, password: password);
+      Navigator.of(context).pushReplacementNamed('home');
+    } catch (error) {
+      errorText = 'Incorrect login or password!';
+      notifyListeners();
+    }
+  }
 }
 
 class LoginPageProvider extends InheritedNotifier {
