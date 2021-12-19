ee21ffe3903413d1ff0652e5e1e982a7255ce71d
beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-12-19 10:45:20 +0600

    is authorized check

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:moovee_land/core/models/app_model.dart';
 import 'app/app.dart';
 
-void main() => runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final _model = AppModel();
+  await _model.checkAuth();
+
+  runApp(App(model: _model));
+}
