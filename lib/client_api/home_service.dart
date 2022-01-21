453d7b8bcc7870fe1ce05d93672e8b382f6b33b4
Beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-01-21 12:32:43 +0600

    home-page service

---
+++ lib/client_api/home_service.dart
@@ -0,0 +1,22 @@
+import 'package:moovee_land/client_api/api_config.dart';
+import 'package:moovee_land/client_api/entity/popular_movies.dart';
+
+class HomeService {
+  Future<PopularMovies> getPopularMovies() async {
+    PopularMovies parser(dynamic json) {
+      final jsonMap = json as Map<String, dynamic>;
+      return PopularMovies.fromJson(jsonMap);
+    }
+
+    final popularMovies = await ApiUtils.get<PopularMovies>(
+      path: '/movie/popular',
+      parser: parser,
+      queryParameters: <String, dynamic>{
+        'api_key': ApiConfig.apiKey,
+        'language': 'en-US',
+        'page': 1,
+      },
+    );
+    return popularMovies;
+  }
+}
