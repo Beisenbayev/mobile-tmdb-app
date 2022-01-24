70b2dfae3e000f9f336c537161a44195243c1454
Beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-01-24 11:06:20 +0600

    handler for searching movies

--- lib/core/models/movies_list_model.dart
+++ lib/core/models/movies_list_model.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:moovee_land/client_api/api_config.dart';
 import 'package:moovee_land/client_api/entity/movie.dart';
+import 'package:moovee_land/client_api/entity/movies_response.dart';
 import 'package:moovee_land/client_api/home_service.dart';
 
 class MoviesListModel extends ChangeNotifier {
@@ -11,22 +13,39 @@ class MoviesListModel extends ChangeNotifier {
   int _currentPageIndex = 0;
   int _totalPageCount = 1;
   bool _isLoadingInProgress = false;
+  String? _searchQuery;
+  Timer? _searchTimer;
 
   List<Movie> get movies => List.unmodifiable(_movies);
 
   MoviesListModel() {
-    _movies.clear();
-    handleLoadMovies();
+    _resetSettings();
   }
 
-  void handleLoadMovies() async {
+  Future<void> _resetSettings() async {
+    _currentPageIndex = 0;
+    _totalPageCount = 1;
+    _movies.clear();
+    await _loadNextPage();
+  }
+
+  Future<MoviesResponse> _loadMovies(int index) async {
+    final String query = _searchQuery ?? '';
+    if (query.isEmpty) {
+      return await _homeService.getPopularMovies(index);
+    } else {
+      return await _homeService.searchMovies(index, query);
+    }
+  }
+
+  Future<void> _loadNextPage() async {
     if (_isLoadingInProgress || _currentPageIndex >= _totalPageCount) return;
 
     _isLoadingInProgress = true;
     final nextPageIndex = _currentPageIndex + 1;
 
     try {
-      final loadedMovies = await _homeService.getPopularMovies(nextPageIndex);
+      final loadedMovies = await _loadMovies(nextPageIndex);
       _currentPageIndex = loadedMovies.page;
       _totalPageCount = loadedMovies.totalPages;
       _movies.addAll(loadedMovies.movies);
@@ -37,9 +56,18 @@ class MoviesListModel extends ChangeNotifier {
     }
   }
 
+  void handleSearchMovies(String text) async {
+    _searchTimer?.cancel();
+    _searchTimer = Timer(const Duration(milliseconds: 600), () async {
+      if (_searchQuery == text) return;
+      _searchQuery = text;
+      await _resetSettings();
+    });
+  }
+
   void loadMoviesByIndex(int index) {
     if (index < movies.length - 1) return;
-    handleLoadMovies();
+    _loadNextPage();
   }
 
   String getImageName(String? path) {
