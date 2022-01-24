5b12ba2907d14f7132c973a420d3965b6b32e1da
Beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-01-24 11:07:24 +0600

    home-service: serch movie function

--- lib/client_api/home_service.dart
+++ lib/client_api/home_service.dart
@@ -19,4 +19,24 @@ class HomeService {
     );
     return response;
   }
+
+  Future<MoviesResponse> searchMovies(int index, String query) async {
+    MoviesResponse parser(dynamic json) {
+      final jsonMap = json as Map<String, dynamic>;
+      return MoviesResponse.fromJson(jsonMap);
+    }
+
+    final response = await ApiUtils.get<MoviesResponse>(
+      path: 'search/movie',
+      parser: parser,
+      queryParameters: <String, dynamic>{
+        'api_key': ApiConfig.apiKey,
+        'language': 'en-US',
+        'page': index.toString(),
+        'query': query,
+        'include_adult': true.toString(),
+      },
+    );
+    return response;
+  }
 }
