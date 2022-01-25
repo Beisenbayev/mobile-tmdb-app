b77629d9b2b300345a6883fb2ab4e3f37182443e
Beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-01-25 11:03:48 +0600

    shows-list in news-feed page was activated

--- lib/core/models/news_feed_model.dart
+++ lib/core/models/news_feed_model.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:moovee_land/client_api/api_config.dart';
 import 'package:moovee_land/client_api/entity/movie.dart';
+import 'package:moovee_land/client_api/entity/show.dart';
 import 'package:moovee_land/client_api/home_service.dart';
 
 class NewsFeedModel extends ChangeNotifier {
@@ -11,15 +12,15 @@ class NewsFeedModel extends ChangeNotifier {
   final List<String> _moviesTypes =
       List.unmodifiable(['Top Rated', 'Popular', 'Now Paying', 'Upcoming']);
   final List<String> _showsTypes =
-      List.unmodifiable(['Top Rated', 'Popular', 'Now Paying', 'Upcoming']);
+      List.unmodifiable(['Top Rated', 'Popular', 'On The Air', 'Airing Today']);
   final List<String> _trandingTypes = List.unmodifiable(['Today', 'Week']);
 
   final List<Movie> _movies = [];
-  final List<Movie> _shows = [];
+  final List<Show> _shows = [];
   final List<Movie> _tranding = [];
 
   List<Movie> get movies => List.unmodifiable(_movies);
-  List<Movie> get shows => List.unmodifiable(_shows);
+  List<Show> get shows => List.unmodifiable(_shows);
   List<Movie> get tranding => List.unmodifiable(_tranding);
 
   List<String> get moviesTypes => _moviesTypes;
@@ -28,9 +29,10 @@ class NewsFeedModel extends ChangeNotifier {
 
   NewsFeedModel() {
     handleChangeMoviesType('Top Rated');
+    handleChangeShowsType('Top Rated');
   }
 
-  Future<List<Movie>> _lodaMovies(String type) async {
+  Future<List<Movie>> _loadMovies(String type) async {
     List<Movie> movies = [];
     switch (type) {
       case 'Top Rated':
@@ -53,10 +55,40 @@ class NewsFeedModel extends ChangeNotifier {
     return movies;
   }
 
+  Future<List<Show>> _loadShows(String type) async {
+    List<Show> shows = [];
+    switch (type) {
+      case 'Top Rated':
+        shows = (await _homeService.getTopRatedShows(1)).shows;
+        break;
+      case 'Popular':
+        shows = (await _homeService.getPopularShows(1)).shows;
+        break;
+      case 'On The Air':
+        shows = (await _homeService.getOnTheAirShows(1)).shows;
+        break;
+      case 'Airing Today':
+        shows = (await _homeService.getAiringTodayShows(1)).shows;
+        break;
+      default:
+        shows = [];
+        break;
+    }
+
+    return shows;
+  }
+
   void handleChangeMoviesType(String type) async {
     _movies.clear();
-    final moviesByType = await _lodaMovies(type);
+    final moviesByType = await _loadMovies(type);
     _movies.addAll(moviesByType);
+    notifyListeners();
+  }
+
+  void handleChangeShowsType(String type) async {
+    _shows.clear();
+    final showsByType = await _loadShows(type);
+    _shows.addAll(showsByType);
     notifyListeners();
   }
 
