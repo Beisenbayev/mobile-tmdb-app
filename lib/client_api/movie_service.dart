1c5e16c2b270441337520920929fbd7203f697f6
Beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-01-27 14:13:39 +0600

    movie-credits entity and service-getter was created

--- lib/client_api/movie_service.dart
+++ lib/client_api/movie_service.dart
@@ -1,15 +1,33 @@
 import 'package:moovee_land/client_api/api_config.dart';
+import 'package:moovee_land/client_api/entity/movie_credits.dart';
 import 'package:moovee_land/client_api/entity/movie_details.dart';
 
 class MovieService {
-  Future<MovieDetails> getMovieDetails(int id) async {
+  Future<MovieDetails> getMovieDetails(int movieId) async {
     MovieDetails parser(dynamic json) {
       final jsonMap = json as Map<String, dynamic>;
       return MovieDetails.fromJson(jsonMap);
     }
 
     final response = await ApiUtils.get<MovieDetails>(
-      path: 'movie/$id',
+      path: 'movie/$movieId',
+      parser: parser,
+      queryParameters: <String, dynamic>{
+        'api_key': ApiConfig.apiKey,
+        'language': 'en-US',
+      },
+    );
+    return response;
+  }
+
+  Future<MovieCredits> getMovieCredits(int movieId) async {
+    MovieCredits parser(dynamic json) {
+      final jsonMap = json as Map<String, dynamic>;
+      return MovieCredits.fromJson(jsonMap);
+    }
+
+    final response = await ApiUtils.get<MovieCredits>(
+      path: 'movie/$movieId/credits',
       parser: parser,
       queryParameters: <String, dynamic>{
         'api_key': ApiConfig.apiKey,
