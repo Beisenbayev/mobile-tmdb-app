b1dd90964f3668ae61012a4933c581e9c52b1eb6
Beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-01-29 09:51:29 +0600

    movie service getter for movie discussions

--- lib/client_api/movie_service.dart
+++ lib/client_api/movie_service.dart
@@ -1,6 +1,7 @@
 import 'package:moovee_land/client_api/api_config.dart';
 import 'package:moovee_land/client_api/entity/movie_credits.dart';
 import 'package:moovee_land/client_api/entity/movie_details.dart';
+import 'package:moovee_land/client_api/entity/movie_discussions.dart';
 
 class MovieService {
   Future<MovieDetails> getMovieDetails(int movieId) async {
@@ -28,6 +29,23 @@ class MovieService {
 
     final response = await ApiUtils.get<MovieCredits>(
       path: 'movie/$movieId/credits',
+      parser: parser,
+      queryParameters: <String, dynamic>{
+        'api_key': ApiConfig.apiKey,
+        'language': 'en-US',
+      },
+    );
+    return response;
+  }
+
+  Future<MovieDiscussions> getMovieDiscussions(int movieId) async {
+    MovieDiscussions parser(dynamic json) {
+      final jsonMap = json as Map<String, dynamic>;
+      return MovieDiscussions.fromJson(jsonMap);
+    }
+
+    final response = await ApiUtils.get<MovieDiscussions>(
+      path: 'movie/$movieId/reviews',
       parser: parser,
       queryParameters: <String, dynamic>{
         'api_key': ApiConfig.apiKey,
